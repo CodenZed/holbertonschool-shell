134f38ff290e7d6b7d9513319934b97419bd9d6c
CodenZed <a.zedhello07@gmail.com>
2026-04-11 17:56:56 +0000

    oop

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Module that contains a class representing a square."""
+"""
+Module that contains a class representing a square.
+"""
 
 
 class Square:
-    """Class that represents a square."""
+    """
+    Class that represents a square.
+    """
     pass
